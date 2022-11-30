5e25fe9fdb68efd1e626bf3c5afd79f5bee42f3c
Ferrari <ferraridev18@gmail.com>
2022-11-29 22:18:26 -0300

    update projeto

---
+++ config/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "localhost";
+$dbname = "teste";
+$user = "teste";
+$pass = "Teste.123";
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+  // ativar o modo de erro
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+  // erro na conexão
+  $error = $e->getMessage();
+  echo "Erro: $error";
+}
