7e7c7c4a4170c18d78dffea8fb1a99963a2ddc5c
Ferrari <ferraridev18@gmail.com>
2022-12-06 22:36:42 -0300

    Edit form

---
+++ edit.php
@@ -0,0 +1,37 @@
+<?php 
+  include_once("templates/header.php");
+  include_once("config/connection.php");
+  include_once("config/process.php");
+
+?>
+
+<div class="container">
+  <?php include_once("templates/backbtn.html"); ?>
+  
+  <h1 id="main-title">Edit Contact</h1>
+  <form id="create-form" action="config/process.php" method="POST">
+    <input type="hidden" name="type" value="update">
+    <input type="hidden" name="id" value="<?= $contact['id'] ?>">
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" class="form-control" id="name" name="name" placeholder="Type the name." value="<?= $contact['name'] ?>" required>
+    </div>
+    <div class="form-group">
+      <label for="phone">Phone:</label>
+      <input type="text" class="form-control" id="phone" name="phone" placeholder="Type the phone." value="<?= $contact['phone'] ?>" required>
+    </div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="text" class="form-control" id="email" name="email" placeholder="Type the email." value="<?= $contact['email'] ?>" required>
+    </div>
+    <div class="form-group">
+      <label for="observation">Obervation:(Optional)</label>
+      <textarea type="text" class="form-control" id="observation" name="observation" placeholder="Type the observation." rows="3"><?= $contact['observation'] ?></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Update</button>
+  </form>
+</div>
+
+<?php 
+  include_once("templates/footer.php");
+?>
--- view.php
+++ view.php
@@ -4,13 +4,16 @@
 ?>
 
   <div class="container" id="view-contact-container">
-   <h1 id="main-title"><?= $contact["name"] ?></h1>
-   <p class="bold">Telefone:</p>
+    <?php include_once("templates/backbtn.html"); ?>
+    <h1 id="main-title"><?= $contact["name"] ?></h1>
+    <p class="bold">Telefone:</p>
    <p><?= $contact["phone"] ?></p>
    <p class="bold">Email:</p>
    <p><?= $contact["email"] ?></p>
+   <p class="bold">Observation:</p>
+   <p><?= $contact["observation"] ?></p>
   </div>
-
-<?php 
+  
+  <?php 
   include_once("templates/footer.php");
-?>
+  ?>
